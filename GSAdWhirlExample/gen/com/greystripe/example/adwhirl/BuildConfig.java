2d4202646d7176723b716f9de5d1e9da99b407ba
Jeffrey Carlson <jeffreypcarlson@gmail.com>
2013-06-06 23:53:34 -0700

    Greystripe Android SDK 2.2.0

--- GSAdWhirlExample/gen/com/greystripe/example/adwhirl/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.greystripe.example.adwhirl;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- GSSDKDemo/gen/com/greystripe/android/GSSDKDemo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.greystripe.android.GSSDKDemo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
